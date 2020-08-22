d68bb3599996306155e2855997ff4e92f65e7f27
vishwas1 <vishwasanand.cse@gmail.com>
2020-08-23 00:44:48 +0530

    removed unwanted files

--- pkiAuth/authServer/dist/controllers/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var registerApp = function (req, res) {
-    res.send('Success');
-};
-exports.registerApp = registerApp;
