8fa6b9a9ddb04ee0cf0f95d9d8fa6a8d907ce7e3
vishwas1 <vishwasanand.cse@gmail.com>
2020-08-23 03:43:16 +0530

    changed folder st.

--- pkiAuth/authServer/src/models/IApplication.ts
+++
@@ -1,8 +0,0 @@
-export default interface IApplication{
-    id: string;
-    appId: string;
-    appSecret: string;
-    isActive: string;
-    name: string;
-    userId: string;
-}
--- pkiAuth/dashboard/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/crypto/'
-    : '/',
-  devServer: {
-      port: 5001
-    }
-}
